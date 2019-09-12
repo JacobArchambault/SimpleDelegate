b4748bdf515057c0cd8d0c2c65911f34691e8a97
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-12 09:53:50 -0400

    Add SimpleMath class, create instance of it, and create a BinaryOp delegate pointing to that instance.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace SimpleDelegate
 {
+
+    // This delegate can point to any method, taking two integers and returning an integer.
+    public delegate int BinaryOp(int x, int y);
+
+    // This class contains methods BinaryOp will point to.
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Simple Delegate Example *****\n");
+
+            // Create an instance of the SimpleMath class, and a BinaryOp delegate object that 'points to' that instance.
+            SimpleMath m = new SimpleMath();
+            BinaryOp b = new BinaryOp(m.Add);
+
+            // Show information about this object.
+            DisplayDelegateInfo(b);
+
+            // Invoke Add() method indirectly using delegate object.
+            Console.WriteLine("10 + 10 is {0}", b(10, 10));
+
+            Console.ReadLine();
+        }
+
+        static void DisplayDelegateInfo(Delegate delObj)
+        {
+            // Print the names of each member in the delegate's invocation list.
+            foreach (Delegate d in delObj.GetInvocationList())
+            {
+                Console.WriteLine("Method Name: {0}", d.Method);
+                Console.WriteLine("Type Name: {0}", d.Target);
+            }
         }
     }
 }
